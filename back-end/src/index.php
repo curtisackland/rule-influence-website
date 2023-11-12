b654e4391ebd469ac59498a601c89f805350d357
Curtman <curtisackland@rogers.com>
2023-11-12 15:52:06 -0500

    Added back-end filter for home page

--- back-end/src/index.php
+++ back-end/src/index.php
@@ -18,11 +18,11 @@ $container = new DI\Container(include __DIR__ . '/../config/di-container.php');
 $app = AppFactory::create(null, $container);
 
 $app->get('/', GetHomePageInfo::class);
-$app->get('/idk', GetOrgPageInfo::class);
-// Test route for messing around
+
 $app->group('/api', function (RouteCollectorProxy $group) {
-    $group->get('/test', TestRoute::class);
+    $group->get('/home', GetHomePageInfo::class);
     $group->get('/organization/{orgName}', GetOrgPageInfo::class);
+    $group->get('/test', TestRoute::class); // REMOVE LATER
 });
 
 $app->run();
--- front-end/src/components/TopOrgCommentersTable.vue
+++ front-end/src/components/TopOrgCommentersTable.vue
@@ -26,7 +26,13 @@
     name: "TopOrgCommentersTable",
     methods: {
       async fetchData() {
-        this.tableData = (await axios.get("http://localhost:8080")).data;
+        this.tableData = (await axios.get("http://localhost:8080/api/home",{
+          params: { filters: {
+              orgName: null, // can be a string of the Organization name
+              sortBy: "yCount", // sort by a specific column: "orgName" || "yCount" || "frdocs" || NULL
+              sortOrder: "highest" // can be "highest" or "lowest"
+            }}
+        })).data;
         this.tableHead = Object.keys(this.tableData[0]);
       }
     },
