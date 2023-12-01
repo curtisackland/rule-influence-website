f475e10a80d09d6e65071552deb5d2ce396cee01
Curtman <curtisackland@rogers.com>
2023-12-01 14:19:29 -0500

    added v-if to frdoc cards

--- front-end/src/views/FRDocs.vue
+++ front-end/src/views/FRDocs.vue
@@ -4,8 +4,10 @@
       <v-card-text>
         <v-row class="d-flex">
           <v-col cols="3">
-            <v-card-title>{{row["frdoc_number"]}}</v-card-title>
-            <v-card-subtitle>{{row["fr_type"]}}</v-card-subtitle>
+            <v-card-title v-if="row['frdoc_number']">{{row["frdoc_number"]}}</v-card-title>
+            <v-card-title v-else>No frdoc number</v-card-title>
+            <v-card-subtitle v-if="row['fr_type']">{{row["fr_type"]}}</v-card-subtitle>
+            <v-card-subtitle v-else>No fr type</v-card-subtitle>
             <v-virtual-scroll class="mx-3 mb-2" :items="row['agencies']">
               <template v-slot:default="{ item }">
                 {{ item }}
@@ -14,9 +16,12 @@
             <v-btn>federalregister.gov</v-btn>
           </v-col>
           <v-col cols="9">
-            <v-card-title>{{ row["title"] }}</v-card-title>
-            <v-card-subtitle>{{ row["publication_date"] }} &bull; {{ row["action"] }}</v-card-subtitle>
-            <v-card-text>{{ row["abstract"] }}</v-card-text>
+            <v-card-title v-if="row['title']">{{ row["title"] }}</v-card-title>
+            <v-card-title v-else>No Title</v-card-title>
+            <v-card-subtitle v-if="row['publication_date'] && row['action']">{{ row["publication_date"] }} &bull; {{ row["action"] }}</v-card-subtitle>
+            <v-card-subtitle v-else>No publication date or action</v-card-subtitle>
+            <v-card-text v-if="row['abstract']">{{ row["abstract"] }}</v-card-text>
+            <v-card-text v-else>No abstract</v-card-text>
           </v-col>
         </v-row>
       </v-card-text>
